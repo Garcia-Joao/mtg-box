7ae76858b8c0c9733e3f5ed628c3cd3e373f8d78
Garcia-Joao <amorimisme@gmail.com>
2025-07-03 14:11:20 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,54 @@
+import './style.css';
+import api from '../../services/api';
+import { useEffect, useState } from 'react';
+
+interface SetData {
+  id: string;
+  name: string;
+  icon_svg_uri: string;
+}
+
+interface ApiResponse {
+  data: SetData[];
+}
+
+function Home() {
+  const [sets, setSets] = useState<SetData[]>([]);
+
+  useEffect(() => {
+    fetchSets();
+  }, []);
+
+  async function fetchSets(): Promise<void> {
+    try {
+      const response = await api.get<ApiResponse>('/sets');
+      setSets(response.data.data);
+    } catch (error) {
+      console.error('Erro ao buscar sets:', error);
+    }
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Sets</h1>
+      </header>
+
+      <main className="container">
+        <br />
+        <div className="scroll-container">
+          {sets.map((set) => (
+            <div key={set.id} className="card">
+              <div className="set-item" title={set.name}>
+                <img src={set.icon_svg_uri} className="set-icon" alt={set.name} />
+                <span className="set-name">{set.name}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Home;
